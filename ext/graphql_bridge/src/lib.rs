c76dbe10366fbc03709b7c4f3de333db464f5a12
Felix Descoteaux <felix.descoteaux@shopify.com>
2023-02-10 15:06:49 -0500

    more complex example with json output

--- ext/graphql_bridge/src/lib.rs
+++ ext/graphql_bridge/src/lib.rs
@@ -1,11 +1,15 @@
 use magnus::{
     method,
+    class,
     Value,
     prelude::*,
     Error, RString, define_class, function,
 };
-use apollo_parser::{Parser, SyntaxTree, ast::{Document, Definition, OperationDefinition, SelectionSet, Selection, Field}};
+use apollo_parser::{Parser, ast::{Definition, OperationDefinition, SelectionSet, Selection, Field}};
 use convert_case::{Case, Casing};
+use serde_json::{json};
+
+type JsonOutput = serde_json::Value;
 
 #[magnus::wrap(class = "GraphQLBridge")]
 struct GraphQLBridge {
@@ -19,53 +23,61 @@ impl GraphQLBridge {
         }
     }
 
-    fn eval(&self, input: RString) -> Result<bool, Error> {
-        let ast = self.parse_input(input)?;
-        self.eval_root(ast.document());
-        Ok(true)
-    }
+    // Parses a GraphQL query injected from Ruby land and evaluates the
+    // resulting AST by calling into a Ruby schema object.
+    fn eval(&self, input: RString) -> Result<String, Error> {
+        let query = unsafe { input.as_str()? };
+        let parser = Parser::new(query);
+        let ast = parser.parse();
+        let mut result = json!({});
 
-    fn eval_root(&self, root: Document) {
-        for def in root.definitions() {
+        for def in ast.document().definitions() {
             if let Definition::OperationDefinition(op_def) = def {
-                self.eval_definition(op_def);
+                self.eval_definition(op_def, self.schema, &mut result);
             }
         }
+
+        Ok(result.to_string())
     }
 
-    fn eval_definition(&self, op_def: OperationDefinition) {
+    fn eval_definition(&self, op_def: OperationDefinition, graph_model: Value, output: &mut JsonOutput) {
         if let Some(set) = op_def.selection_set() {
-            self.eval_selection_set(set);
+            self.eval_selection_set(set, graph_model, output);
         }
     }
 
-    fn eval_selection_set(&self, selection_set: SelectionSet) {
+    fn eval_selection_set(&self, selection_set: SelectionSet, graph_model: Value, output: &mut JsonOutput) {
         for selection in selection_set.selections() {
-            self.eval_selection(selection);
+            self.eval_selection(selection, graph_model, output);
         }
     }
 
-    fn eval_selection(&self, selection: Selection) {
+    fn eval_selection(&self, selection: Selection, graph_model: Value, output: &mut JsonOutput) {
         match selection {
             Selection::Field(field) => {
-                self.eval_field(field);
+                self.eval_field(field, graph_model, output);
             },
             _ => {}
         }
     }
 
-    fn eval_field(&self, field: Field) {
+    fn eval_field(&self, field: Field, graph_model: Value, output: &mut JsonOutput) {
         if let Some(name) = field.name() {
-            let text = name.text().as_str().to_case(Case::Snake);
-            let _value: Result<Value, magnus::Error> = self.schema.funcall(text, ());
-        }
-    }
+            let name = name.text();
+            let value: Value = graph_model.funcall(name.to_case(Case::Snake), ()).unwrap();
 
-    fn parse_input(&self, input: RString) -> Result<SyntaxTree, Error> {
-        let input = unsafe { input.as_str()? };
-        let parser = Parser::new(input);
-        let ast = parser.parse();
-        Ok(ast)
+            if value.is_kind_of(class::string()) {
+                output[name.as_str()] = serde_json::Value::String(value.to_string());
+            } else {
+                output[name.as_str()] = json!({});
+
+                self.eval_selection_set(
+                    field.selection_set().unwrap(),
+                    value,
+                    &mut output[name.as_str()],
+                );
+            }
+        }
     }
 }
 
--- test/graphql_bridge_test.rb
+++ test/graphql_bridge_test.rb
@@ -1,22 +1,49 @@
 # frozen_string_literal: true
 
+require 'json'
 require "test/unit"
 require_relative "../lib/graphql_bridge"
 
 
 class Schema
+  class SomeObject
+    def some_other_field
+      "some other value"
+    end
+  end
+
+  def initialize
+    @some_object = SomeObject.new
+  end
+
   def some_field
-    puts "resolving some field"
+    "some value"
+  end
+
+  def some_object
+    @some_object
   end
 end
 
 class GraphQLBridgeTest < Test::Unit::TestCase
   def test_it_works
     bridge = GraphQLBridge.new(Schema.new)
-    assert(bridge.eval(<<~GQL))
+    result = bridge.eval(<<~GQL)
       {
         someField
+        someObject {
+          someOtherField
+        }
       }
     GQL
+
+    expected = {
+      "someField" => "some value",
+      "someObject" => {
+        "someOtherField" => "some other value"
+      }
+    }.to_json
+
+    assert_equal(expected, result)
   end
 end
